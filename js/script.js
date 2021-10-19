f77374aedd49f8c48ad96297bdbfdbea7856906e
Hal <hgrforrest@gmail.com>
2021-10-19 16:28:19 -0400

    more styling, changing colors, adding green circles, etc :green_circle: :go: :cat:

--- js/script.js
+++ js/script.js
@@ -35,17 +35,34 @@ app.navigateTo = (event, goTo) => {
 
 // app initialization
 app.init = function() {
+	app.aboutEl.classList.add('currentButton')
 	app.aboutEl.addEventListener('click', (event) => {
 		app.navigateTo(event, 'aboutContent')
+		app.aboutEl.classList.add('currentButton')
+		app.skillsEl.classList.remove('currentButton')
+		app.projectsEl.classList.remove('currentButton')
+		app.contactEl.classList.remove('currentButton')
 	})
 	app.skillsEl.addEventListener('click', (event) => {
 		app.navigateTo(event, 'skillsContent')
+		app.skillsEl.classList.add('currentButton')
+		app.aboutEl.classList.remove('currentButton')
+		app.projectsEl.classList.remove('currentButton')
+		app.contactEl.classList.remove('currentButton')
 	})
 	app.projectsEl.addEventListener('click', (event) => {
 		app.navigateTo(event, 'projectsContent')
+		app.projectsEl.classList.add('currentButton')
+		app.skillsEl.classList.remove('currentButton')
+		app.aboutEl.classList.remove('currentButton')
+		app.contactEl.classList.remove('currentButton')
 	})
 	app.contactEl.addEventListener('click', (event) => {
 		app.navigateTo(event, 'contactContent')
+		app.contactEl.classList.add('currentButton')
+		app.skillsEl.classList.remove('currentButton')
+		app.projectsEl.classList.remove('currentButton')
+		app.aboutEl.classList.remove('currentButton')
 	})
 }
 
